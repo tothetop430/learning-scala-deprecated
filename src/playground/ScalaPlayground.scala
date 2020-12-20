02f6704864f1f007e204cb7ba2a696492b53b6c8
tothetop430 <jhuseyrer355545@gmail.com>
2020-12-20 17:43:19 +0530

    update tail recursion exercises

--- src/playground/ScalaPlayground.scala
+++ src/playground/ScalaPlayground.scala
@@ -1,5 +1,48 @@
 package playground
 
+import scala.annotation.tailrec
+
 object ScalaPlayground extends App {
-  println("Hello, Scala")
+  println("Hello, Scala Playground")
+
+  // Tail recursive string concatenation
+  def concatenateStrings(text: String, n: Int): String = {
+    @tailrec
+    def csHelper(temp: String, x: Int, acc: String): String = {
+      if (x <= 0) acc
+      else csHelper(temp, x-1, acc+temp)
+    }
+    csHelper(text, n, "")
+  }
+  println("concatenateStrings tail recursive")
+  println(concatenateStrings("Hello", 3000))
+
+  // IsPrime tail recursive
+  def isPrime(n: Int): Boolean = {
+    @tailrec
+    def ipHelper(x: Int, isPrimeUntil: Boolean): Boolean = {
+      if (!isPrimeUntil) false
+      else if (x <= 1) true
+      else ipHelper(x - 1, n % x != 0 && isPrimeUntil)
+    }
+    ipHelper(n/2, true)
+  }
+
+  println("isPrime tail recursive")
+  println(isPrime(97643317))
+
+  // Fibonacci Tail Recursive
+  def fibonacci(n: Int): Int = {
+    @tailrec
+    def fiboHelper(x: Int, f: Int, s: Int): Int = {
+      if (x<=2) s
+      else fiboHelper(x-1, s, f+s)
+    }
+
+    fiboHelper(n, 1, 1)
+  }
+
+  println("fibonacci tail recursive")
+  println(fibonacci(3))
+
 }
