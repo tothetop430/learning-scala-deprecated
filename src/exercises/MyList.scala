815435b82c88f2d8f20971a924b4a2ac9682cc14
tothetop430 <jhuseyrer355545@gmail.com>
2021-02-02 14:51:22 +0530

    update mylist

--- src/exercises/MyList.scala
+++ src/exercises/MyList.scala
@@ -1,28 +1,28 @@
 package exercises
 
-abstract class MyList {
+abstract class MyList[+A] {
 
-  def head: Int
-  def tail: MyList
+  def head: A
+  def tail: MyList[A]
   def isEmpty: Boolean
-  def add(element: Int): MyList
+  def add[B >: A](element: B): MyList[B]
   def printElements: String
   override def toString(): String = "[" + printElements + "]"
 }
 
-object EmptyList extends MyList {
-  def head: Int = throw new NoSuchElementException
-  def tail: MyList = throw new NoSuchElementException
+object EmptyList extends MyList[Nothing] {
+  def head: Nothing = throw new NoSuchElementException
+  def tail: Nothing = throw new NoSuchElementException
   def isEmpty: Boolean = true
-  def add(element: Int): MyList = new Cons(element, EmptyList)
+  def add[B >: Nothing](element: B): MyList[B] = new Cons(element, EmptyList)
   def printElements: String = ""
 }
 
-class Cons(h: Int, t: MyList) extends MyList {
-  def head: Int = h
-  def tail: MyList = t
+class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
+  def head: A = h
+  def tail: MyList[A] = t
   def isEmpty: Boolean = false
-  def add(element: Int): MyList = new Cons(element, this)
+  def add[B >: A](element: B): MyList[B] = new Cons(element, this)
   def printElements: String = {
     if(t.isEmpty) "" + h
     else h + " " + t.printElements
@@ -31,9 +31,8 @@ class Cons(h: Int, t: MyList) extends MyList {
 
 object Test extends App {
   val list = new Cons(1, new Cons(2, new Cons(3, EmptyList)))
-  println(list.head)
-  println(list.tail.head)
-  println(list.add(4).head)
-  println(list.isEmpty)
+  val listStr = new Cons("Hello", new Cons("Scala", EmptyList))
+
   println(list.toString())
+  println(listStr.toString())
 }
