bb237e437937888e22635fc7f20d7e09cb8f8155
tothetop430 <jhuseyrer355545@gmail.com>
2021-02-04 19:05:14 +0530

    create HOFAndCurries

---
+++ src/lectures/part3fp/HOFAndCurries.scala
@@ -0,0 +1,16 @@
+package lectures.part3fp
+
+object HOFAndCurries extends App {
+
+  def nTimes(f: Int => Int, n: Int, x: Int): Int = {
+    if (n<=0) x
+    else nTimes(f, n-1, f(x))
+  }
+
+  def someUtility(x: Int): Int = x * 2
+
+  println(nTimes(someUtility, 10, 2))
+
+
+
+}
