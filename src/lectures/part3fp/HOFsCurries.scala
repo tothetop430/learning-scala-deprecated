d9b7e9d20e17ee9e5ba56839c2d773772f601eea
tothetop430 <jhuseyrer355545@gmail.com>
2021-05-05 13:49:57 +0530

    update HOFsCurries.scala

--- src/lectures/part3fp/HOFsCurries.scala
+++ src/lectures/part3fp/HOFsCurries.scala
@@ -27,4 +27,52 @@ object HOFsCurries extends App {
   println(standardFormat(Math.PI))
   println(preciseFormat(Math.PI))
 
+  /*
+  Exercises
+
+  1. Expand MyList
+    - foreach method A => Unit
+      [1,2,3].foreach(x => println(x))
+
+    - sort function ((A,A) => Int) => MyList
+      [1,2,3].sort((x, y) => y - x) => [3,2,1]
+
+    - zipWith(list, (A, A) => B) => MyList[B]
+      [1,2,3].zipWith([4,5,6], x*y) => [4, 10, 18]
+
+    - fold(start)(function) => a value
+      [1,2,3].fold(0)(x + y) => 6
+
+  2. toCurry(f: (Int => Int) => Int): Int => Int => Int
+     fromCurry(f: Int => Int => Int): (Int => Int) => Int
+
+  3. Composition of functions
+     compose(f, g) => f(g(x))
+     aCompose(f, g) => g(f(x))
+
+   */
+
+  def toCurry(f: (Int, Int) => Int): (Int => Int => Int) =
+    x => y => f(x, y)
+
+  def fromCurry(f: (Int => Int => Int)): (Int, Int) => Int =
+    (x, y) => f(x)(y)
+
+  def compose[T, A, B](f: A => B, g: T => A): T => B =
+    x => f(g(x))
+
+  def aCompose[A, B, T](f: A => B, g: B => T): A => T =
+    x => g(f(x))
+
+  val valueAdder: Int => Int => Int = toCurry(_ + _)
+  println(valueAdder(2)(3))
+
+  val simpleAdder: (Int, Int) => Int = fromCurry(valueAdder)
+  println(simpleAdder(2, 3))
+
+  def add1(x: Int): Int = x + 2
+  def mul1(x: Int): Int = x * 3
+
+  println(compose(add1, mul1)(4))
+  println(aCompose(add1, mul1)(4))
 }
