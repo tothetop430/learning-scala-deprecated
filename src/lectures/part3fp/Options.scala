916c0317c43a4da3f6773a01e36b419bbc6a40ce
tothetop430 <jhuseyrer355545@gmail.com>
2021-05-07 12:18:39 +0530

    update options.scala

--- src/lectures/part3fp/Options.scala
+++ src/lectures/part3fp/Options.scala
@@ -1,5 +1,7 @@
 package lectures.part3fp
 
+import scala.util.Random
+
 object Options extends App {
 
   val someOption: Option[Int] = Some(1)
@@ -26,8 +28,58 @@ object Options extends App {
 
   println(betterUnSafeMethod() orElse betterSafeMethod())
 
-  val option1 = Some()
+  val option1 = Some(12)
   val option2 = None
   println(option1.isEmpty)
   println(option2.isEmpty)
+
+  println(option1.get)
+  println(option1.map(_ + 2))
+  println(option1.filter(_ < 10))
+  println(option1.flatMap(x => Option(x * 2)))
+
+  /*
+  Exercise
+   */
+
+  val config: Map[String, String] = Map(
+    "host" -> "127.0.0.1",
+    "port" -> "80"
+  )
+
+  class Connection {
+    def connect = "Connected"
+  }
+  object Connection {
+    val random = new Random(System.nanoTime())
+    def apply(host: String, port: String): Option[Connection] = {
+      if (random.nextBoolean()) Some(new Connection)
+      else None
+    }
+  }
+
+  // try to establish a connection - if so call connect method
+  val host = config.get("host")
+  val port = config.get("port")
+  val connection = host.flatMap(h => port.flatMap(p => Connection(h, p)))
+  val connectionStatus = connection.map(x => x.connect)
+  println(connectionStatus)
+  connectionStatus.foreach(println)
+
+  // 2 Method
+  println("2nd method")
+  config.get("host")
+    .flatMap(host => config.get("port")
+      .flatMap(port => Connection(host, port)))
+    .map(connection => connection.connect)
+    .foreach(println)
+
+  // 3 Method
+  println("3rd method")
+  val aConnectStatus = for {
+    host <- config.get("host")
+    port <- config.get("port")
+    connection <- Connection(host, port)
+  } yield connection.connect
+  println(aConnectStatus)
 }
